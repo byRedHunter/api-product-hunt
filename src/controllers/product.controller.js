304ed1d1db9e57b02c60c1b9d9e7b1f84d87a99c
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-09-07 10:06:56 -0500

    crear producto

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,10 +1,31 @@
-const { resError } = require('../utils/response')
+const Product = require('../models/Product')
+const cloudinary = require('../utils/cloudinary')
+const { resError, resSuccess } = require('../utils/response')
 
 exports.createProduct = async (req, res) => {
 	// verificar el tipo de archivo
 	if (req.fileValidator) return resError(res, 400, req.fileValidator)
 
-	return res.json({ msg: 'vamos bien' })
+	try {
+		// subimos el archivo a cloudinary
+		const resultUploadImage = await cloudinary.uploader.upload(req.file.path)
+
+		// creamos el nuevo producto
+		const product = new Product({
+			...req.body,
+			image: resultUploadImage.secure_url,
+			cloudinaryId: resultUploadImage.public_id,
+			author: req.user.id,
+		})
+
+		// guardamos en la db
+		await product.save()
+
+		return resSuccess(res, product)
+	} catch (error) {
+		console.log(error)
+		return resError(res, 500, 'Error al crear el post.')
+	}
 }
 
 exports.getAllProducts = async (req, res) => {}
